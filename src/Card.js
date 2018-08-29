3ea4db370afac408ead9f0f63de0b0b421699678
AcAntellAno <acantellano1996@gmail.com>
2018-08-29 13:59:31 -0700

    Added card data to frontend

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
-const Card = () => {
+const Card = props => {
   return (
-    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img
-        src="https://avatars1.githubusercontent.com/u/24821806?s=200&v=4"
-        alt="Members"
-      />
+    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img src={`${props.image}`} alt="Members" />
       <div>
-        <h2>AcAntellAno</h2>
-        <p>AcAntellAno@github.com</p>
+        <h2>Username: {props.username}</h2>
+        <p>Team: {props.teamname}</p>
+        <p>Github: {props.profile}</p>
       </div>
     </div>
   );
--- src/Members.js
+++ src/Members.js
@@ -2,113 +2,112 @@ export const members = [{
         id: 1,
         username: 'AcAntellAno',
         teamname: 'grocerhub',
-        image: 'https://avatars1.githubusercontent.com/u/24821806?s=460&v=4',
+        image: 'https://avatars1.githubusercontent.com/u/24821806?s=420&v=4',
         link: 'https://github.com/AcAntellAno'
     },
     {
         id: 2,
         username: '2940cristian',
         teamname: 'mentorship',
-        image: 'https://avatars1.githubusercontent.com/u/24821806?s=460&v=4',
+        image: 'https://avatars0.githubusercontent.com/u/36054337?s=460&v=4',
         link: 'https://github.com/2940cristian'
     },
     {
         id: 3,
         username: 'bparkerproductions',
         teamname: 'mentorship',
-        image: 'https://avatars1.githubusercontent.com/u/14989482?s=460&v=4',
+        image: 'https://avatars1.githubusercontent.com/u/14989482?s=420&v=4',
         link: 'https://github.com/bparkerproductions'
     },
     {
         id: 4,
         username: 'DeepInkDesign',
         teamname: 'NA',
-        image: 'https://avatars1.githubusercontent.com/u/42124478?s=460&v=4',
+        image: 'https://avatars1.githubusercontent.com/u/42124478?s=420&v=4',
         link: 'https://github.com/DeepInkDesign'
     },
     {
         id: 5,
         username: 'epichi123',
         teamname: 'mentorship',
-        image: 'https://avatars0.githubusercontent.com/u/17955032?s=460&v=4',
+        image: 'https://avatars0.githubusercontent.com/u/17955032?s=420&v=4',
         link: 'https://github.com/epichi123'
     },
     {
         id: 6,
         username: 'fangweb',
         teamname: 'grocerhub',
-        image: 'https://avatars2.githubusercontent.com/u/28660913?s=460&v=4',
+        image: 'https://avatars2.githubusercontent.com/u/28660913?s=420&v=4',
         link: 'https://github.com/fangweb'
     },
     {
         id: 7,
         username: 'garrison1234',
         teamname: 'lit',
-        image: 'https://avatars2.githubusercontent.com/u/35623693?s=460&v=4',
+        image: 'https://avatars2.githubusercontent.com/u/35623693?s=420&v=4',
         link: 'https://github.com/garrison1234'
     },
     {
         id: 8,
         username: 'JonathanFillion',
         teamname: 'lit',
-        image: 'https://avatars1.githubusercontent.com/u/31373537?s=460&v=4',
+        image: 'https://avatars1.githubusercontent.com/u/31373537?s=420&v=4',
         link: 'https://github.com/JonathanFillion'
     },
     {
         id: 9,
         username: 'Kremor Pärt',
         teamname: 'lit',
-        image: 'https://avatars0.githubusercontent.com/u/13699437?s=460&v=4',
+        image: 'https://avatars0.githubusercontent.com/u/13699437?s=420&v=4',
         link: 'https://github.com/Kremor'
     },
     {
         id: 10,
         username: 'NorthSouthCoding',
         teamname: 'mentorship',
-        image: 'https://avatars0.githubusercontent.com/u/23747503?s=460&v=4',
+        image: 'https://avatars0.githubusercontent.com/u/23747503?s=420&v=4',
         link: 'https://github.com/NorthSouthCoding'
     },
     {
         id: 11,
         username: 'rchs819',
         teamname: 'grocerhub',
-        image: 'https://avatars2.githubusercontent.com/u/35659961?s=460&v=4',
+        image: 'https://avatars2.githubusercontent.com/u/35659961?s=420&v=4',
         link: 'https://github.com/rchs819'
     },
     {
         id: 12,
         username: 'rickywid',
         teamname: 'grocerhub',
-        image: 'https://avatars1.githubusercontent.com/u/12060263?s=460&v=4',
+        image: 'https://avatars1.githubusercontent.com/u/12060263?s=420&v=4',
         link: 'https://github.com/rickywid'
     },
     {
         id: 13,
         username: 'rmjordas',
         teamname: 'grocerhub',
-        image: 'https://avatars2.githubusercontent.com/u/27814962?s=460&v=4',
+        image: 'https://avatars2.githubusercontent.com/u/27814962?s=420&v=4',
         link: 'https://github.com/rmjordas'
     },
     {
         id: 14,
         username: 'roymoran',
         teamname: 'mentorship',
-        image: 'https://avatars2.githubusercontent.com/u/4195689?s=460&v=4',
+        image: 'https://avatars2.githubusercontent.com/u/4195689?s=420&v=4',
         link: 'https://github.com/roymoran'
     },
     {
         id: 15,
         username: 'ThatsRicci',
         teamname: 'lit',
-        image: 'https://avatars1.githubusercontent.com/u/770550?s=460&v=4',
+        image: 'https://avatars1.githubusercontent.com/u/770550?s=420&v=4',
         link: 'https://github.com/ThatsRicci'
     },
     {
         id: 16,
         username: 'theboynextdoor',
         teamname: 'lit',
-        image: 'https://avatars2.githubusercontent.com/u/28246170?s=460&v=4',
+        image: 'https://avatars2.githubusercontent.com/u/28246170?s=420&v=4',
         link: 'https://github.com/theboynextdoor'
     }
-
 ]
