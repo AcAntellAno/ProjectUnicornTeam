a2e02736729d75b18701f1566be525420cf9de04
AcAntellAno <acantellano1996@gmail.com>
2018-08-29 13:37:49 -0700

    Finished adding member information

--- src/Members.js
+++ src/Members.js
@@ -7,93 +7,108 @@ export const members = [{
     },
     {
         id: 2,
-        username: 'AcAntellAno',
-        teamname: 'grocerhub',
-        link: 'https://github.com/AcAntellAno'
+        username: '2940cristian',
+        teamname: 'mentorship',
+        image: 'https://avatars1.githubusercontent.com/u/24821806?s=460&v=4',
+        link: 'https://github.com/2940cristian'
     },
     {
         id: 3,
-        username: 'AcAntellAno',
-        teamname: 'grocerhub',
-        link: 'https://github.com/AcAntellAno'
+        username: 'bparkerproductions',
+        teamname: 'mentorship',
+        image: 'https://avatars1.githubusercontent.com/u/14989482?s=460&v=4',
+        link: 'https://github.com/bparkerproductions'
     },
     {
         id: 4,
-        username: 'AcAntellAno',
-        teamname: 'grocerhub',
-        link: 'https://github.com/AcAntellAno'
+        username: 'DeepInkDesign',
+        teamname: 'NA',
+        image: 'https://avatars1.githubusercontent.com/u/42124478?s=460&v=4',
+        link: 'https://github.com/DeepInkDesign'
     },
     {
         id: 5,
-        username: 'AcAntellAno',
-        teamname: 'grocerhub',
-        link: 'https://github.com/AcAntellAno'
+        username: 'epichi123',
+        teamname: 'mentorship',
+        image: 'https://avatars0.githubusercontent.com/u/17955032?s=460&v=4',
+        link: 'https://github.com/epichi123'
     },
     {
         id: 6,
-        username: 'AcAntellAno',
+        username: 'fangweb',
         teamname: 'grocerhub',
-        link: 'https://github.com/AcAntellAno'
+        image: 'https://avatars2.githubusercontent.com/u/28660913?s=460&v=4',
+        link: 'https://github.com/fangweb'
     },
     {
         id: 7,
-        username: 'AcAntellAno',
-        teamname: 'grocerhub',
-        link: 'https://github.com/AcAntellAno'
+        username: 'garrison1234',
+        teamname: 'lit',
+        image: 'https://avatars2.githubusercontent.com/u/35623693?s=460&v=4',
+        link: 'https://github.com/garrison1234'
     },
     {
         id: 8,
-        username: 'AcAntellAno',
-        teamname: 'grocerhub',
-        link: 'https://github.com/AcAntellAno'
+        username: 'JonathanFillion',
+        teamname: 'lit',
+        image: 'https://avatars1.githubusercontent.com/u/31373537?s=460&v=4',
+        link: 'https://github.com/JonathanFillion'
     },
     {
         id: 9,
-        username: 'AcAntellAno',
-        teamname: 'grocerhub',
-        link: 'https://github.com/AcAntellAno'
+        username: 'Kremor Pärt',
+        teamname: 'lit',
+        image: 'https://avatars0.githubusercontent.com/u/13699437?s=460&v=4',
+        link: 'https://github.com/Kremor'
     },
     {
         id: 10,
-        username: 'AcAntellAno',
-        teamname: 'grocerhub',
-        link: 'https://github.com/AcAntellAno'
+        username: 'NorthSouthCoding',
+        teamname: 'mentorship',
+        image: 'https://avatars0.githubusercontent.com/u/23747503?s=460&v=4',
+        link: 'https://github.com/NorthSouthCoding'
     },
     {
         id: 11,
-        username: 'AcAntellAno',
+        username: 'rchs819',
         teamname: 'grocerhub',
-        link: 'https://github.com/AcAntellAno'
+        image: 'https://avatars2.githubusercontent.com/u/35659961?s=460&v=4',
+        link: 'https://github.com/rchs819'
     },
     {
         id: 12,
-        username: 'AcAntellAno',
+        username: 'rickywid',
         teamname: 'grocerhub',
-        link: 'https://github.com/AcAntellAno'
+        image: 'https://avatars1.githubusercontent.com/u/12060263?s=460&v=4',
+        link: 'https://github.com/rickywid'
     },
     {
         id: 13,
-        username: 'AcAntellAno',
+        username: 'rmjordas',
         teamname: 'grocerhub',
-        link: 'https://github.com/AcAntellAno'
+        image: 'https://avatars2.githubusercontent.com/u/27814962?s=460&v=4',
+        link: 'https://github.com/rmjordas'
     },
     {
         id: 14,
-        username: 'AcAntellAno',
-        teamname: 'grocerhub',
-        link: 'https://github.com/AcAntellAno'
+        username: 'roymoran',
+        teamname: 'mentorship',
+        image: 'https://avatars2.githubusercontent.com/u/4195689?s=460&v=4',
+        link: 'https://github.com/roymoran'
     },
     {
         id: 15,
-        username: 'AcAntellAno',
-        teamname: 'grocerhub',
-        link: 'https://github.com/AcAntellAno'
+        username: 'ThatsRicci',
+        teamname: 'lit',
+        image: 'https://avatars1.githubusercontent.com/u/770550?s=460&v=4',
+        link: 'https://github.com/ThatsRicci'
     },
     {
         id: 16,
-        username: 'AcAntellAno',
-        teamname: 'grocerhub',
-        link: 'https://github.com/AcAntellAno'
+        username: 'theboynextdoor',
+        teamname: 'lit',
+        image: 'https://avatars2.githubusercontent.com/u/28246170?s=460&v=4',
+        link: 'https://github.com/theboynextdoor'
     }
 
 ]
